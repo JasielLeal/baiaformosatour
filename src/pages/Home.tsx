9230c76511243d79fd3bea198130747359501623
Jasiel <jasieloficial@hotmail.com>
2024-04-19 21:49:22 -0300

    fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,8 +5,8 @@ import { Pacotes } from "@/components/Pacotes";
 
 
 import bannerv4 from '/bannerv4.png'
-import iphone from '/iphone4.png'
-import iphone3 from '/iphone3.png'
+import iphone from '/iphone5.png'
+import iphone3 from '/iphone5.png'
 
 export function Home() {
     return (
@@ -19,7 +19,7 @@ export function Home() {
             <div>
                 <img src={bannerv4} alt="bannerv4" className="h-[70] hidden md:block"/>
                 <img src={iphone} alt="bannerv4" className="h-[70]  table:hidden cover"/>
-                <img src={iphone3} alt="bannerv4" className="h-[70]  hidden table:block md:hidden"/>
+                <img src={iphone3} alt="bannerv4" className="h-[70]  hidden table:block md:hidden w-full cover"/>
             </div>
             <Loca />
 
